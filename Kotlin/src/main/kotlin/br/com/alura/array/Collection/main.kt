3d59e48139a61c93cf54e7970835515d692ecf2f
Vitor Duarte Passo <vitor.passo@codex.com.br>
2023-03-13 13:06:53 -0300

    [learn] learning about how add, remove and put valeu in Map with kotlin

--- Kotlin/src/main/kotlin/br/com/alura/array/Collection/main.kt
+++ Kotlin/src/main/kotlin/br/com/alura/array/Collection/main.kt
@@ -24,5 +24,23 @@ fun main() {
 	for(pedidoCardapio in cardapioParis){
 		println("Para o prato '${pedidoCardapio.key}' temos a seguinte descrição: ${pedidoCardapio.value}")
 	}
+	// para fazer um mapa é preciso de um pair no kotlin
+	val pedidosMap = mutableMapOf(1 to 13.0, 2 to 50.0)
+	println("Antes de adicionar um novo pedido $pedidosMap")
+	pedidosMap[3] = 45.0
+	println("Depois de adicionar $pedidosMap")
+
+	//adicionando com o put
+	pedidosMap.put(4, 50.0)
+	println("Adicionando com o put $pedidosMap")
+	//atualizando com o put
+	pedidosMap.put(1, 12.0)
+	println("Atualizando com o put $pedidosMap")
+	pedidosMap.putIfAbsent(2, 23.2)
+	//a linha 39 não irá executar, pois na chave 2 já existe um valor
+	// e o 'putIfAbsent só adiciona um valor caso aquela determinada chava ainda não esteja preechida'
+	println(pedidosMap)
+	pedidosMap.putIfAbsent(5, 32.0)
+	println("Adicionando um valor a uma determinada chave que ainda não se tem um valor $pedidosMap")
 }
 
