c8388089761dfeb1be2c22080c2267199f9d26ac
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2023-03-15 00:42:06 -0300

    [learn] learn associate in Map and Collection

--- Kotlin/src/main/kotlin/br/com/alura/array/Collection/main.kt
+++ Kotlin/src/main/kotlin/br/com/alura/array/Collection/main.kt
@@ -1,9 +1,27 @@
 package br.com.alura.array.Collection
 
 fun main() {
-	testComportamentoMap()
+	val pedidos = listOf(
+		Pedido(1, 15.0),
+		Pedido(2, 25.0),
+		Pedido(3, 35.0),
+		Pedido(4, 45.0),
+		Pedido(5, 55.0),
+		Pedido(6, 65.0),
+	)
+	println(pedidos)
+	val pedidosAssociados = pedidos.associateBy { pedido -> pedido.numero }
+	println(pedidosAssociados[1])
+
+	val pedidosComFrete = pedidos.associateWith { pedido ->
+		pedido.valor >= 45.0
+	}
+	println(pedidosComFrete)
+	println(pedidosComFrete[Pedido(numero=1, valor=15.0)])
 
 }
+
+data class Pedido(val numero: Int, val valor: Double)
 
 
 
